daadccac17fbf9b131228298e15bd1cdd4cdb969
CJ Caraang <156409258+CJCJsC@users.noreply.github.com>
2024-03-04 21:27:09 -1000

    Section 4

--- app/imports/ui/Task.jsx
+++ app/imports/ui/Task.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
 
-export const Task = ({ task }) => {
-  return <li>{task.text}</li>
+export const Task = ({ task, onCheckboxClick, onDeleteClick }) => {
+  return (
+
+      <li>
+        <span>{task.text}</span>
+        <button onClick={() => onDeleteClick(task)}>&times;</button>
+        <input
+            type="checkbox"
+            checked={!!task.isChecked}
+            onClick={() => onCheckboxClick(task)}
+            readOnly
+        />
+        <span>{task.text}</span>
+        <ul>
+          {tasks.map(task => <Task
+              key={task._id}
+              task={task}
+              onCheckboxClick={toggleChecked}
+              onDeleteClick={deleteTask}
+          />)}
+        </ul>
+      </li>
+
+  );
 };
